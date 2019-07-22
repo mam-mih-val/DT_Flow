694a3a22624c52fdd1851bfc67821688ec711d1a
mam-mih-val <mam.mih.val@gmail.com>
2019-07-22 18:24:39 +0300

    Comparasing

--- src/CorrectionTask.cpp
+++ src/CorrectionTask.cpp
@@ -9,219 +9,118 @@
 namespace Qn {
 
 CorrectionTask::CorrectionTask(std::string inFile, std::string incalib) :
-    out_file_(new TFile("output.root", "RECREATE")),
-    in_calibration_file_(new TFile(incalib.data(), "READ")),
-    out_calibration_file_(new TFile("qn.root", "RECREATE")),
-    out_tree_(nullptr),
-    manager_(),
-    write_tree_(true) 
+	out_file_(new TFile("output.root", "RECREATE")),
+	in_calibration_file_(new TFile(incalib.c_str(), "READ")),
+	out_calibration_file_(new TFile("qn.root", "RECREATE")),
+	out_tree_(nullptr),
+	fManager(),
+	write_tree_(true) ,
+	fEvent{nullptr}
 {
-  femtoReader = new StFemtoDstReader(inFile.c_str());
-  femtoReader->Init();
-  femtoReader->SetStatus("*",0);
-  femtoReader->SetStatus("Event",1);
-  femtoReader->SetStatus("Track",1);
-  if( !femtoReader->chain() ) {
-    std::cout << "No chain has been found." << std::endl;
-  }
-  out_file_->cd();
-  out_tree_ = new TTree("tree", "tree");
+	fChain = new TChain("DataTree");
+    fChain->Add(inFile.c_str());
+	std::cout << "File read. " << fChain->GetEntries() << " events found." << endl;
+    fChain->SetBranchAddress("DTEvent", &fEvent);
+	std::cout << "Branch read successfully" << std::endl;
+	fSelector = new Selector(fEvent);
+	out_file_->cd();
+	out_tree_ = new TTree("tree", "tree");
+	std::cout << "Construction of Qn::CorrectionTask ended successfully" << endl;
 }
 
-void CorrectionTask::Run() {
-  Initialize();
-  QnCorrectionsSetTracingLevel(kError);
-  std::cout << "Processing..." << std::endl;
-  
-  Long64_t events2read = femtoReader->chain()->GetEntries();
-
-  for(Long64_t iEvent=0; iEvent<events2read; iEvent++) {
-    if( !femtoReader->readFemtoEvent(iEvent) ) break;
-    StFemtoDst *dst = femtoReader->femtoDst();
-    event = dst->event();
-    if( !event )  break;
-    if ( !isGoodEvent(event) ) continue;
-    Process();
-  }
-  
-  std::cout << "Analyzed " << events2read << " events." << std::endl;
-  Finalize();
-}
-
-Bool_t CorrectionTask::isGoodEvent(StFemtoEvent *const &event)
+void CorrectionTask::Run() 
 {
-  if (!event) return false;
-  if (event == nullptr) return false;
-
-  if (event->primaryVertex().Perp() > cutVtxR) return false;
-  if (TMath::Abs(event->primaryVertex().Z()) > cutVtxZEnergy.at(energy)) return false;
-
-  if ((energy == 200.) && TMath::Abs(event->primaryVertex().Z() - event->vpdVz()) > cutVpdVz) return false;
-
-  return true;
-}
-
-Bool_t CorrectionTask::isGoodTrack(StFemtoTrack *const &track, Float_t _energy, TVector3 pVtx)
-{
-  if (!track) return false;
-  if (!track->isPrimary()) return false;
-  if (track->nHitsFit() < cutNhits) return false;
-  if (track->nHitsPoss() <= cutNhitsPoss) return false;
-  if ((Double_t) track->nHitsFit()/track->nHitsPoss() < cutNhitsRatio) return false;
-  if (TMath::Abs(track->eta()) >= cutEta) return false;
-  if (track->pt() <= cutPtMin.at(_energy)) return false;
-  if (track->ptot() > cutPMax) return false;
-  if (track->gDCA(pVtx).Mag() >= cutDCA.at(_energy)) return false;
-  return true;
+	Initialize();
+	QnCorrectionsSetTracingLevel(kError);
+	std::cout << "Processing..." << std::endl;
+	
+	Long64_t numEvents = fChain->GetEntries();
+	for(Long64_t idx=0; idx<numEvents; idx++) 
+	{
+		fChain->GetEntry(idx);
+		if( !fSelector->IsCorrectEvent() )
+			continue;
+		Process();
+	}
+	Finalize();
 }
 
 void CorrectionTask::Initialize() {
-  // Add Variables to variable manager needed for filling
-  manager_.AddVariable("Centrality", VAR::kCentrality, 1);
-  manager_.AddVariable("Vx", VAR::kVx, 1);
-  manager_.AddVariable("Vy", VAR::kVy, 1);
-  manager_.AddVariable("Vz", VAR::kVz, 1);
-  manager_.AddVariable("Pt", VAR::kPt, 1);
-  manager_.AddVariable("Eta", VAR::kEta, 1);
-  manager_.AddVariable("Phi", VAR::kPhi, 1);
-  manager_.AddVariable("Dca", VAR::kDca, 1);
-  manager_.AddVariable("Pid", VAR::kPid, 1);
-  manager_.AddVariable("BBCwestPhi", VAR::kBBCwestPhi, 16);
-  manager_.AddVariable("BBCeastPhi", VAR::kBBCeastPhi, 16);
-  manager_.AddVariable("BBCwestADC", VAR::kBBCwestADC, 16);
-  manager_.AddVariable("BBCeastADC", VAR::kBBCeastADC, 16);
+	cout << "Initialization began" << endl;
+	// Add Variables to variable manager needed for filling
+	fManager.AddVariable("Centrality", kCentrality, 1);
+	fManager.AddVariable("FwRing", kFwModuleRing, 1);
+	fManager.AddVariable("FwModuleId", kFwModuleId, 1);
+	fManager.AddVariable("FwAdc", kFwModuleAdc, 1);
+	fManager.AddVariable("FwPhi", kFwModulePhi, 1);
+	std::cout << "Variables added" << std::endl;
+	//Correction eventvariables
+	fManager.SetEventVariable("Centrality");
+	fManager.AddCorrectionAxis({"Centrality", 9, 0, 45});
 
-  //Correction eventvariables
-  manager_.SetEventVariable("Centrality");
-  manager_.AddCorrectionAxis({"Centrality", 20, 0, 100});
+	//Configuration of FW. Preparing for add axis to qa histograms
+	Axis Centrality("Centrality", {0.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0});
+  
+	//Producing the function which will configurate the correction Manager
+	auto FwConfiguration = [](DetectorConfiguration *config) 
+	{
+		config->SetNormalization(QVector::Normalization::M);
+		auto recenter = new Recentering();
+		config->AddCorrectionOnQnVector(recenter);
+		auto rescale = new TwistAndRescale();
+		rescale->SetApplyTwist(true);
+		rescale->SetApplyRescale(true);
+		rescale->SetTwistAndRescaleMethod(TwistAndRescale::TWRESCALE_doubleHarmonic);
+		config->AddCorrectionOnQnVector(rescale);
+	};
+	auto Is1se = [](double ring){ return ring>0 && ring <=4; };
+	auto Is2se = [](double ring){ return ring==5 || ring ==6; };
+	auto Is3se = [](double ring){ return ring>=7 && ring <=9; };
+	// 3 sub-events method.
+	// Each detector builds own Q-vector, which means, you need to add required count of detectors and then configurate their cuts.  
+	fManager.AddDetector("Fw1", DetectorType::CHANNEL, "FwPhi", "FwAdc", {Centrality}, {1});
+	fManager.AddCut("Fw1", {"FwRing"}, Is1se);
+	fManager.SetCorrectionSteps("Fw1", FwConfiguration);
 
-  //Config TPC
-  Axis pt("Pt", {0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1., 1.25, 1.5, 1.75, 2.0, 2.25, 2.5});
-  Axis etaEast("Eta", 5, -1., 0.);
-  Axis etaWest("Eta", 5, 0., 1.);
+	fManager.AddDetector("Fw2", DetectorType::CHANNEL, "FwPhi", "FwAdc", {Centrality}, {1});
+	fManager.AddCut("Fw2", {"FwRing"}, Is2se);
+	fManager.SetCorrectionSteps("Fw2", FwConfiguration);
 
-//  auto cut_eta = [](const double &eta) { return -1. < eta && eta < 1.; };
+	fManager.AddDetector("Fw3", DetectorType::CHANNEL, "FwPhi", "FwAdc", {Centrality}, {1});
+	fManager.AddCut("Fw3", {"FwRing"}, Is3se);
+	fManager.SetCorrectionSteps("Fw3", FwConfiguration);
   
-  //Config of TPC corrections
-  auto confTPC = [](DetectorConfiguration *config) {
-    config->SetNormalization(QVector::Normalization::M);
-    auto recenter = new Recentering();
-    config->AddCorrectionOnQnVector(recenter);
-    auto rescale = new TwistAndRescale();
-    rescale->SetApplyTwist(true);
-    rescale->SetApplyRescale(true);
-    rescale->SetTwistAndRescaleMethod(TwistAndRescale::TWRESCALE_doubleHarmonic);
-    config->AddCorrectionOnQnVector(rescale);
-  };
-  
-  // TPC pT-dependence
-  manager_.AddDetector("pion_east_pT", DetectorType::TRACK, "Phi", "Pt", {pt}, {1, 2, 3, 4});
-  manager_.AddCut("pion_east_pT", {"Eta", "Dca", "Pid"}, [](const double &eta, const double &dca, const double &pid) { return pid == 211 && -1. < eta && eta < 0. && dca < 1.; });
-  manager_.SetCorrectionSteps("pion_east_pT", confTPC);
-  
-  manager_.AddDetector("pion_west_pT", DetectorType::TRACK, "Phi", "Pt", {pt}, {1, 2, 3, 4});
-  manager_.AddCut("pion_west_pT", {"Eta", "Dca", "Pid"}, [](const double &eta, const double &dca, const double &pid) { return pid == 211 && 0. < eta && eta < 1. && dca < 1.; });
-  manager_.SetCorrectionSteps("pion_west_pT", confTPC);
-  
-  // TPC eta-dependence
-  manager_.AddDetector("pion_east_eta", DetectorType::TRACK, "Phi", "Pt", {etaEast}, {1, 2, 3, 4});
-  manager_.AddCut("pion_east_eta", {"Eta", "Dca", "Pid"}, [](const double &eta, const double &dca, const double &pid) { return pid == 211 && -1 < eta && eta < 0. && dca < 1.; });
-  manager_.SetCorrectionSteps("pion_east_eta", confTPC);
-  
-  manager_.AddDetector("pion_west_eta", DetectorType::TRACK, "Phi", "Pt", {etaWest}, {1, 2, 3, 4});
-  manager_.AddCut("pion_west_eta", {"Eta", "Dca", "Pid"}, [](const double &eta, const double &dca, const double &pid) { return pid == 211 && 0. < eta && eta < 1. && dca < 1.; });
-  manager_.SetCorrectionSteps("pion_west_eta", confTPC);
+	fManager.AddEventHisto1D({{"Centrality", 20, 0, 100}});
+	fManager.SetTree(out_tree_);
+	cout << "93 line" << endl;
+	fManager.Initialize(in_calibration_file_);
+	cout << "95 line" << endl;
 
-  //TPC integrated
-  manager_.AddDetector("TPCeast", DetectorType::TRACK, "Phi", "Pt", {}, {1, 2, 3, 4});
-  manager_.AddCut("TPCeast", {"Pt", "Eta"}, [](const double &pt, const double &eta) { return pt < 2. && eta < -0.05; });
-  manager_.SetCorrectionSteps("TPCeast", confTPC);
-  
-  manager_.AddDetector("TPCwest", DetectorType::TRACK, "Phi", "Pt", {}, {1, 2, 3, 4});
-  manager_.AddCut("TPCwest", {"Pt", "Eta"}, [](const double &pt, const double &eta) { return pt < 2. && eta > 0.05; });
-  manager_.SetCorrectionSteps("TPCwest", confTPC);
-  
-  std::vector <std::string> TPC_subevents = {"pion_east_pT", "pion_west_pT", "pion_east_eta", "pion_west_eta", "TPCeast", "TPCwest"};
-  for (auto &sub : TPC_subevents)
-  {
-    manager_.AddHisto2D(sub, {{"Pt", 50, 0., 10.}, {"Phi", 50, 0, 2*TMath::Pi()}});
-    manager_.AddHisto2D(sub, {{"Eta", 50, -1., 1.}, {"Phi", 50, 0, 2*TMath::Pi()}});
-    manager_.AddHisto2D(sub, {{"Eta", 50, -1., 1.}, {"Pt", 50, 0., 10.}});
-  }
-  
-  //Config of BBC corrections
-  auto confBBC = [](DetectorConfiguration *config) {
-    config->SetNormalization(QVector::Normalization::M);
-    auto recenter = new Recentering();
-//    recenter->SetApplyWidthEqualization(true);
-    config->AddCorrectionOnQnVector(recenter);
-    auto BBCchannels = new bool[16];
-    auto BBCchannelGroups = new int[16];
-    for (int ich = 0; ich < 16; ich++) {
-      BBCchannels[ich] = true;
-      BBCchannelGroups[ich] = ich/1;
-    }
-    config->SetChannelsScheme(BBCchannels, BBCchannelGroups);
-//    auto equal = new GainEqualization();
-//    equal->SetEqualizationMethod(GainEqualization::Method::AVERAGE);
-//    equal->SetUseChannelGroupsWeights(true);
-//    config->AddCorrectionOnInputData(equal);
-  };
-
-  //BBC
-  auto cut_ADC = [](double &adc) {
-    return adc > -0.1;
-  };
-  
-  manager_.AddDetector("BBCwest", DetectorType::CHANNEL, "BBCwestPhi", "BBCwestADC", {}, {1, 2, 3, 4});
-  manager_.AddCut("BBCwest", {"BBCwestADC"}, cut_ADC);
-  manager_.SetCorrectionSteps("BBCwest", confBBC);
-  
-  manager_.AddDetector("BBCeast", DetectorType::CHANNEL, "BBCeastPhi", "BBCeastADC", {}, {1, 2, 3, 4});
-  manager_.AddCut("BBCeast", {"BBCeastADC"}, cut_ADC);
-  manager_.SetCorrectionSteps("BBCeast", confBBC);
-  
-  std::vector <std::string> BBC_subevents = {"BBCwest", "BBCeast"};
-  for (auto &sub : BBC_subevents)
-  {
-//    manager_.AddHisto1D(sub, {{"BBCwestChannels", 16, 0, 16}}, "BBCwestADC");
-//    manager_.AddHisto1D(sub, {{"BBCwestChannels", 16, 0, 16}}, "BBCwestPhi");
-//    manager_.AddHisto2D(sub, {{"BBCwestPhi", 100, -TMath::Pi(), TMath::Pi()}, {"BBCwestChannel", 16, 0, 16}}, "BBCwestADC");
-  }                      
-  
-  manager_.AddEventHisto1D({{"Centrality", 20, 0, 100}});
-  manager_.AddEventHisto1D({{"Vz", 200, -30, 30}});
-  manager_.AddEventHisto2D({{"Vx", 200, -0.5, 0.5}, {"Vy", 200, -0.5, 0.5}});
-  
-  manager_.SetTree(out_tree_);
-  manager_.Initialize(in_calibration_file_);
 }
 
 void CorrectionTask::Process() {
-  manager_.Reset();
-  StFemtoEvent *event = dst->event();
-  VAR::FillEventInfo(event, manager_.GetVariableContainer());
-  manager_.ProcessEvent();
-  manager_.FillChannelDetectors();
-  
-  Int_t nTracks = dst->numberOfTracks();
-  TVector3 pVtx = event->primaryVertex();
+	fManager.Reset();
+	auto varContainer = fManager.GetVariableContainer(); 
+	varContainer[kCentrality]=fEvent->GetCentrality(HADES_constants::kNhitsTOF_RPC_cut);
+	fManager.ProcessEvent();
+	Int_t nModules = fEvent->GetNPSDModules();
 
-  for(Int_t iTrk=0; iTrk<nTracks; iTrk++) {
-    StFemtoTrack *femtoTrack = dst->track(iTrk);
-    if (!femtoTrack) continue;
-    if ( !femtoTrack->isPrimary() ) continue;
-    if (!isGoodTrack(femtoTrack, energy, pVtx)) continue;
-    if ( femtoTrack->isTofTrack() ) {}
-    VAR::FillTrackInfo(femtoTrack, pVtx, manager_.GetVariableContainer());
-    manager_.FillTrackingDetectors();
-  }
-  manager_.ProcessQnVectors();
+	for(Int_t idx=0; idx<nModules; idx++) 
+	{
+		if( !fSelector->IsCorrectFwHit(idx, 0, "adc", 80.0) )
+			continue;
+		varContainer[kFwModuleRing]=(double)fEvent->GetPSDModule(idx)->GetRing();
+		varContainer[kFwModuleId]=	(double)fEvent->GetPSDModule(idx)->GetId();
+		varContainer[kFwModuleAdc]=	(double)fEvent->GetPSDModule(idx)->GetEnergy();
+		varContainer[kFwModulePhi]=	(double)fEvent->GetPSDModule(idx)->GetPhi();
+		fManager.FillChannelDetectors();
+	}
+	fManager.ProcessQnVectors();
 }
 
 void CorrectionTask::Finalize() {
-  femtoReader->Finish();
-  manager_.Finalize();
-  manager_.SaveOutput(out_calibration_file_, out_file_);
+	fManager.Finalize();
+	fManager.SaveOutput(out_calibration_file_, out_file_);
+	std::cout << "Successfully Finalized." << std::endl;
 }
 }
--- src/CorrectionTask.h
+++ src/CorrectionTask.h
@@ -9,20 +9,14 @@
 #include <array>
 #include <random>
 
+#include "CorrectionManager.h"
+#include "Selector.h"
 #include "TFile.h"
 #include "TTree.h"
+#include "TChain.h"
 #include "TTreeReader.h"
 
-#include "StFemtoDstReader.h"
-#include "StFemtoDst.h"
-#include "StFemtoEvent.h"
-#include "StFemtoTrack.h"
-
-#include "FemtoDstVarManager.h"
-#include "CorrectionManager.h"
-
-
-#define VAR FemtoDstVarManager
+#include "HADES_constants.h"
 
 namespace Qn {
 /**
@@ -30,57 +24,45 @@ namespace Qn {
  * @brief TestTask for analysing qn vectors
  */
 class CorrectionTask {
- public:
-  CorrectionTask(std::string filelist, std::string incalib);
-  ~CorrectionTask() = default;
-  void Run();
+	public:
+	CorrectionTask(std::string filelist, std::string incalib);
+	~CorrectionTask() = default;
+	void Run();
 
- private:
-  /**
-   * Initializes TestTask;
-   */
-  void Initialize();
-  /**
-   * Processes one event;
-   */
-  void Process();
-  /**
-   * Finalizes TestTask. Called after processing is done.
-   */
-  void Finalize();
-  
-  Bool_t isGoodEvent(StFemtoEvent *const &event);
-  Bool_t isGoodTrack(StFemtoTrack *const &track, Float_t _energy, TVector3 pVtx);
-  
- protected:
-  std::shared_ptr<TFile> out_file_;
-  std::shared_ptr<TFile> in_calibration_file_;
-  std::shared_ptr<TFile> out_calibration_file_;
-  StFemtoDst *dst;
-  StFemtoEvent *event;
-  StFemtoDstReader* femtoReader;
-  TTree *out_tree_;
-  TTreeReader tree_reader_;
-  Qn::CorrectionManager manager_;
-  bool write_tree_;
-  
-  // Used constants (global)
-  const Int_t energy = 200.;
+	private:
+	/**
+	 * Initializing function of Correction Task class. Makes configuration of Correction Manager.
+	 */
+	void Initialize();
+	/**
+	 * Processes one event;
+	 */
+	void Process();
+	/**
+	 * Finalizes TestTask. Called after processing is done.
+	 */
+	void Finalize();
 
-  // Used constants for the event cut
-  const std::map<Float_t, Double_t> cutVtxZEnergy = {{7.7, 70.}, {11.5, 50.}, {19.6, 70}, {27., 70.}, {39., 40.}, {62., 40.}, {200., 30.}};
-  const Double_t cutVtxR = 2.;
-  const Double_t cutVpdVz = 3.;
+	protected:
 
-  // Used constants for the track cut
-  const std::map<Float_t, Double_t> cutDCA = {{7.7, 1.}, {11.5, 1.}, {19.6, 1.}, {27., 1.}, {39., 1.}, {62., 1.}, {200., 3.}};
-  const Double_t cutEta = 1.;
-  const Int_t    cutNhits = 15;
-  const Double_t cutNhitsPoss = 0.;
-  const Double_t cutNhitsRatio = 0.51;
-  const std::map<Float_t, Double_t> cutPtMin = {{7.7, 0.2}, {11.5, 0.2}, {19.6, 0.2}, {27., 0.2}, {39., 0.2}, {62., 0.2}, {200., 0.15}};
-  const Double_t cutPMax = 10.;
-
+	enum Vars{
+		kCentrality=0,
+		kFwModuleRing,
+		kFwModuleId,
+		kFwModuleAdc,
+		kFwModulePhi,
+		kNumberOfVars
+	};
+	std::shared_ptr<TFile> out_file_;
+	std::shared_ptr<TFile> in_calibration_file_;
+	std::shared_ptr<TFile> out_calibration_file_;
+	TTree *out_tree_;
+	TBranch* DTEvent;
+	DataTreeEvent* fEvent;
+	TChain* fChain;
+	Selector* fSelector;
+	Qn::CorrectionManager fManager;
+	bool write_tree_;
 };
 }
 #endif //FLOW_QNTASK_H
