ea283b0bc6b0027e3bf3c699e32d10b6755102e4
mam-mih-val <mam.mih.val@gmail.com>
2019-09-16 16:56:05 +0200

    Runnig all configuration

--- Runner/run_configuration.sh
+++ Runner/run_configuration.sh
@@ -3,10 +3,10 @@
 name=$1
 
 dirname=gt80$name
-./batch.sh --signal adc --min 80 --max 999 file.list ../Output/dirname
+./batch.sh --signal adc --min 80 --max 999 file.list ../Output/$dirname
 
 dirname=gt80lt120$name
-./batch.sh --signal adc --min 80 --max 120 file.list ../Output/dirname
+./batch.sh --signal adc --min 80 --max 120 file.list ../Output/$dirname
 
 dirname=gt120$name
-./batch.sh --signal adc --min 120 --max 999 file.list ../Output/dirname
+./batch.sh --signal adc --min 120 --max 999 file.list ../Output/$dirname
