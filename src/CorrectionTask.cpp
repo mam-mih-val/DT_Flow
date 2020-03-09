10a89e315f83ac5e4145fb3113e38486bcfcc2f7
mam-mih-val <mam.mih.val@gmail.com>
2020-03-09 21:07:13 +0300

    un-corrections are switched off for RND-Sub

--- src/CorrectionTask.cpp
+++ src/CorrectionTask.cpp
@@ -318,13 +318,13 @@ void CorrectionTask::InitializeRnd() {
   auto MdcConfiguration = [](DetectorConfiguration *config) {
     config->SetNormalization(QVector::Normalization::M);
     auto recenter = new Recentering();
-    config->AddCorrectionOnQnVector(recenter);
+//    config->AddCorrectionOnQnVector(recenter);
     auto rescale = new TwistAndRescale();
     rescale->SetApplyTwist(true);
     rescale->SetApplyRescale(true);
     rescale->SetTwistAndRescaleMethod(
         TwistAndRescale::TWRESCALE_doubleHarmonic);
-    config->AddCorrectionOnQnVector(rescale);
+//    config->AddCorrectionOnQnVector(rescale);
   };
 
   // Configuration of FW. Preparing for add axis to qa histograms
