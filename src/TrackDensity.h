a31a7a76ba9b311bf75aaf073370a36ab33aee4b
mam-mih-val <mam.mih.val@gmail.com>
2020-03-25 15:27:14 +0300

    Different pt-bin

--- src/TrackDensity.h
+++ src/TrackDensity.h
@@ -65,7 +65,7 @@ public:
         if (event_->GetVertexTrack(idx)->GetPdgId() != pid_code_)
           continue;
         auto p = event_->GetVertexTrack(idx)->GetMomentum();
-        if( -0.55 < p.Rapidity()-Y_BEAM/2 && p.Rapidity()-Y_BEAM/2 < 0.55 && 0.5 < p.Pt() && p.Pt() < 0.6 )
+        if( -0.15 < p.Rapidity()-Y_BEAM/2 && p.Rapidity()-Y_BEAM/2 < 0.15 && 0.2 < p.Pt() && p.Pt() < 0.3 )
           occupancy_maps_.at((float)centrality_.GetCentrality())->Fill(p.Phi() - psi, p.Theta());
       }
     } catch (const std::exception &e) {
