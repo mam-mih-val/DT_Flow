67b84c84ae5c2d4f5706817deb53a806395f13f6
mam-mih-val <mam.mih.val@gmail.com>
2020-04-04 19:25:03 +0300

    Optimization window changed

--- src/CorrectionTask.cpp
+++ src/CorrectionTask.cpp
@@ -517,7 +517,7 @@ void CorrectionTask::InitializeRndOptimization(){
       [referencePid](const double &y, const double &pid, const double &pt) {
         return -0.45 < y && y < 0.45 &&
         pid == referencePid &&
-        0.6 < pt && pt < 1.4;
+        0.4 < pt && pt < 1.4;
       });
   fManager.SetCorrectionSteps("TracksMdc", MdcConfiguration);
 
